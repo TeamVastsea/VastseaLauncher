0e93c7e61c369ec98e7137e6e77050d5494b0fc5
GaoNeng-wWw <gaonenggg@outlook.com>
2023-07-17 20:12:24 +0800

    feat: login todo

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,32 +1,10 @@
-import React, {useEffect, useState } from 'react';
-import { appWindow } from '@tauri-apps/api/window';
-import { invoke } from '@tauri-apps/api';
-import api from '../api';
+import React from 'react';
 
 
 export default function Home(){
-	const [code, setCode] = useState('');
-	appWindow.listen('oauth', (e) => {
-		const uri = e.payload as string;
-		const url = new URL(uri);
-		if (url.href.includes('https://mccteam.github.io/redirect.html#code=')){
-			setCode(
-				url.href.replace('https://mccteam.github.io/redirect.html#code=', '')
-			);
-			invoke('destory_oauth_window');
-		}
-	});
 	const login = async () => {		
-		await invoke('oauth');
+		// TODO: invoke rust command.
 	};
-	useEffect(()=>{
-		if (code){
-			api.auth.ms(code)
-				.then((res) => {
-					console.log(res.data);
-				});
-		}
-	}, [code]);
 	return (
 		<div className='mx-auto my-auto w-52 h-52 flex flex-col'>
 			<div className='
