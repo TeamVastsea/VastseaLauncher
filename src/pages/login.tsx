09d915d2c378415a68c9558779c99435d72f0e10
zrll12 <zhengruilin0@126.com>
2023-07-19 03:16:51 +0800

    refactor: move get credit code

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,31 +1,12 @@
-import React, {useEffect, useState } from 'react';
-import { appWindow } from '@tauri-apps/api/window';
+import React from 'react';
 import { invoke } from '@tauri-apps/api';
-import {debug} from '../api/log';
 
 
 export default function Login(){
-	const [code, setCode] = useState('');
-	appWindow.listen('oauth', (e) => {
-		const uri = e.payload as string;
-		const url = new URL(uri);
-		if (url.href.includes('https://mccteam.github.io/redirect.html#code=')){
-			setCode(
-				url.href.replace('https://mccteam.github.io/redirect.html#code=', '')
-			);
-			invoke('auth_window_destroy').then(() => {
-				debug('destroyed');
-			});
-		}
-	});
 	const login = async () => {		
 		await invoke('auth_window_create');
 	};
-	useEffect(()=>{
-		if (code){
-			invoke('auth_credential_get', {code: code}).then((res) => console.log(res));
-		}
-	}, [code]);
+	
 	return (
 		<div className='mx-auto my-auto w-52 h-52 flex flex-col'>
 			<div className='
