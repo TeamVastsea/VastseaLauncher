8082788a09a9e758cf6d01311964bec6814410a4
GaoNeng-wWw <gaonenggg@outlook.com>
2023-07-28 16:37:43 +0800

    ✨ feat(src): 对齐toaster

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {createStore, atom, Provider} from 'jotai';
+import {createStore, atom, Provider } from 'jotai';
 
 import './assets/css/main.css';
 
@@ -12,8 +12,34 @@ import { invoke } from '@tauri-apps/api';
 
 const store = createStore();
 export const settingAtom = atom<Partial<Setting>>({});
+export const tokenAtom = atom<AuthCredentials>({
+	access_token: '',
+	refresh_token: '',
+	vastsea_token: ''
+});
+export const authAtom = atom<boolean>(false);
+export const profileAtom = atom({
+	mojang:{
+		user_name: 'Snowball_233',
+		uuid: 'fc54dd5b70914e3d88663181ee517d5b'
+	},
+	vastsea: {
+		_id: '',
+		ban_reason: '',
+		bind_qq: -1,
+		display_name: '',
+		enabled: false,
+		group: []
+	}
+} as Profile);
 
 store.set(settingAtom, {});
+store.set(tokenAtom, {
+	access_token: '',
+	refresh_token: '',
+	vastsea_token: ''
+});
+store.set(authAtom, false);
 
 store.sub(settingAtom, () => {
 	invoke('update_setting', {setting: store.get(settingAtom)});
@@ -27,7 +53,9 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 				<div className='w-[800px] h-[600px] flex flex-col bg-[url("public/img/bg.png")] bg-cover rounded-[12px]'>
 					<NavBar />
 					<App />
-					<Toaster />
+					<Toaster containerClassName='w-[800px] h-[calc(600px_-_30px)] mx-auto mt-[30px] top-0' containerStyle={{
+						inset: '0'
+					}} />
 				</div>
 			</BrowserRouter>
 		</Provider>
