a3c1805d7224a999c2db61ae319032b0dc79ef95
GaoNeng-wWw <gaonenggg@outlook.com>
2023-08-13 14:50:23 +0800

    feat: window drag

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -12,7 +12,7 @@ export default function NavBar(){
 		<div className="
 			flex w-full h-[30px] font-Instrument_sans text-white bg-[rgba(0,0,0,.25)] backdrop-blur-2lg
 			text-xs leading-1 items-center rounded-t-xl
-		">
+		" data-tauri-drag-region>
 			<div className='flex-grow-1 flex-shrink-0'>
 				<span className='ml-3'>VASTSEA Launcher</span>
 			</div>
